e4181243e27908cb9ac7b39731c00e5ed6ca7823
Konrad Malawski <konrad.malawski@project13.pl>
2013-07-23 01:35:48 +0100

    remove wrong keybind

--- xmonad.hs
+++ xmonad.hs
@@ -34,6 +34,7 @@ import XMonad.Layout.Fullscreen
 
 import XMonad.Util.EZConfig
 import XMonad.Util.Run
+import XMonad.Util.Font
 
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.SetWMName
@@ -220,7 +221,6 @@ myKeyBindings =
     , ((0, 0x1008FF12), spawn "amixer -q set Master toggle")
     , ((0, 0x1008FF11), spawn "amixer -q set Master 10%-")
     , ((0, 0x1008FF13), spawn "amixer -q set Master 10%+")
-    , ((0, 0x1008FF13), spawn "kdesudo pm-hibernate")
   ]
 
 
